22c747f9f60e3ea6b1345519fcdf3a792552b199
beckie fang <beckie@verizonmedia.com>
2020-08-10 17:05:52 +0800

    tutorial-2020-08-10

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const {
     PORT = 5466,
 } = process.env;
 
-const TARGET_INTROSPECT_URL = ''; // TODO
-const TARGET_QUERY_URL = ''; // TODO
+const TARGET_INTROSPECT_URL = 'https://abumedia-graviton-gateway-apac.media.yahoo.com/api/v1/admin/introspect'; // TODO
+const TARGET_QUERY_URL = 'https://abumedia-graviton-gateway-apac.media.yahoo.com/api/v1/gql/query'; // TODO
 
 const app = express();
 
@@ -18,12 +18,26 @@ app.get('/', (req, res) => {
     res.send('hello world');
 });
 
-app.get('/playground', (req, res) => {
-    // TODO
-});
+app.get('/playground', expressPlayground({
+    endpoint: '/graphql'
+}));
 
-app.post('/graphql', (req, res) => {
+app.post('/graphql', async (req, res) => {
     // TODO
+    // console.log('req:', req.body);
+    const { operationName, query, variables, body } = req.body;
+    if (operationName === 'IntrospectionQuery') {
+        const result = await axios.get(TARGET_INTROSPECT_URL);
+        return res.send(result.data);
+    }
+
+    const result = await axios.get(TARGET_QUERY_URL, {
+        params: {
+            query,
+            variables
+        }
+    })
+    res.send(result.data);
 });
 
 app.listen(PORT, () => {
